115fba20bdcef9792bcc780f8b4835182baadbf7
faikxsahin <faikxsahin@outlook.com>
2024-03-27 10:39:33 +0300

    Added change color on trigger feature to Player

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
 
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+
     [SerializeField] float destroyDelay = 0.5f;
 
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
 
     void OnCollisionEnter2D(Collision2D other) 
     {
@@ -23,6 +35,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
 
@@ -30,6 +43,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
 
     }
