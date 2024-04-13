115ac907d69f3c4721fecbd2168d2d88d6cd25ce
faikxsahin <faikxsahin@outlook.com>
2024-04-13 17:02:59 +0300

    Added speed up and slow down mechanism

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -7,11 +7,8 @@ public class Driver : MonoBehaviour
 
     [SerializeField] float steerSpeed = 300f;
     [SerializeField] float moveSpeed = 20f;
-
-    void Start()
-    {
-
-    }
+    [SerializeField] float slowSpeed = 10f;
+    [SerializeField] float boostSpeed = 30f;
 
     void Update()
     {
@@ -23,5 +20,22 @@ public class Driver : MonoBehaviour
         transform.Translate(0, moveAmount, 0);
 
     }
-    
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+        moveSpeed = slowSpeed;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
+
+    }
+
 }
